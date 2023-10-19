26d7686a32ba2e2cc032bcf0e78910fcf80cd924
Mohamed Mohamed <mohamed.sep92@gmail.com>
2023-10-19 14:43:01 +0300

    hw1 lesson 2

--- hw-basic-java/hw1/Homework.java
+++ hw-basic-java/hw1/Homework.java
@@ -1,39 +1,44 @@
 package lesson2.homework;
 
 public class Homework {
-        /*
-          Section 1. We don’t do it in the developmen
-          1)
-          mercedes = (1 + (3*(2-4))%3 + (11-7)*2)+(3-5)*3;
-          volvo = mercedes*3 - 9%mercedes + 16-(-mercedes);
-          opel = mercedes%volvo;
-          Values need to be calculated mersedes, volvo и opel.
-          
-          2)
-          int x = 3;
-          int y = ++x*2;
-          x+=-y*3 + ++x + --y;
-          y+=(-x++)%3;
-          int z = x + y;
-          Values need to be calculated x, y и z.
-         
-          3)
-          boolean a = (85/5 + (19-25%3))==(-3)*(-2+14/(-2))&&(2%7-1)*3<=2;
-          boolean b = !a||a&&!a;
-          boolean c = false&&!true||!((23%3+14)%3==7);
-          Values need to be calculated a, b и c.
-          
-          
-          Separator 2. We do it in the development env
-          
-          4)
-          Create two type variables int:
-          int a = 7;
-          int b = 9;
-          Change the values of variables a и b.
-          You cannot create new variables.
-          You can only apply arithmetic operations to variables a и b.
-          Print the values of variables a and b to the console after they have 
-          been redefined.
-        */
+  /*
+   * Section 1. We don’t do it in the development
+   * 1)
+   * mercedes = (1 + (3*(2-4))%3 + (11-7)*2)+(3-5)*3;
+   * volvo = mercedes*3 - 9%mercedes + 16-(-mercedes);
+   * opel = mercedes%volvo;
+   * Values need to be calculated mersedes, volvo и opel.
+   * 
+   * 
+   * 
+   * 2)
+   * int x = 3;
+   * int y = ++x*2;
+   * x+=-y*3 + ++x + --y;
+   * y+=(-x++)%3;
+   * int z = x + y;
+   * Values need to be calculated x, y и z.
+   * 
+   * 
+   * 
+   * 3)
+   * boolean a = (85/5 + (19-25%3))==(-3)*(-2+14/(-2))&&(2%7-1)*3<=2;
+   * boolean b = !a||a&&!a;
+   * boolean c = false&&!true||!((23%3+14)%3==7);
+   * Values need to be calculated a, b и c.
+   * 
+   * 
+   * 
+   * Separator 2. We do it in the development env
+   * 
+   * 4)
+   * Create two type variables int:
+   * int a = 7;
+   * int b = 9;
+   * Change the values of variables a и b.
+   * You cannot create new variables.
+   * You can only apply arithmetic operations to variables a и b.
+   * Print the values of variables a and b to the console after they have
+   * been redefined.
+   */
 }
