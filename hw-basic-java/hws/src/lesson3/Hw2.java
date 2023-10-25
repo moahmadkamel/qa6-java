0e035685dda53e2018f01c10d8a9052dd9b386a2
Mohamed Mohamed <mohamed.sep92@gmail.com>
2023-10-25 04:05:29 +0300

    hw2

--- hw-basic-java/hws/src/lesson3/Hw2.java
+++ hw-basic-java/hws/src/lesson3/Hw2.java
@@ -1,5 +1,63 @@
+
 public class Hw2 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("");
+    public static void main(String[] args) {
+        String str1 = "This line that i want to cut, cause it is too long";
+        System.out.println("str1 = " + str1 + ". length = " + str1.length());
+        // This line that i want to cut, cause it is too long
+        String str2 = str1.substring(0, str1.length() - 15);
+        // This line that i want to cut, cause
+        System.out.println("str2 = " + str2 + ". length = " + str2.length());
+        String str3 = str2.replace(" i ", " don't ").concat(" it is perfect");
+        // This line that don't want to cut, cause it is perfect
+        System.out.println("str3 = " + str3 + ". length = " + str3.length());
+        ///////////////////////
+
+        System.out.println("-----------------------------");
+        String jobString = "Testing, is my favorite job";
+        String[] arrayOfStrings = jobString.split("\\s");
+
+        String wordString1 = arrayOfStrings[0].replace(",", "");
+        int wordLength1 = wordString1.length();
+        System.out.println("word1 = ( " + wordString1 + " )" + ", length of this word = ( " + wordLength1 + " )");
+
+        String wordString2 = arrayOfStrings[1];
+        int wordLength2 = wordString2.length();
+        System.out.println("word2 = ( " + wordString2 + " )" + ", length of this word = ( " + wordLength2 + " )");
+
+        String wordString3 = arrayOfStrings[2];
+        int wordLength3 = wordString3.length();
+        System.out.println("word3 = ( " + wordString3 + " )" + ", length of this word = ( " + wordLength3 + " )");
+
+        String wordString4 = arrayOfStrings[3];
+        int wordLength4 = wordString4.length();
+        System.out.println("word4 = ( " + wordString4 + " )" + ", length of this word = ( " + wordLength4 + " )");
+
+        String wordString5 = arrayOfStrings[4];
+        int wordLength5 = wordString5.length();
+        System.out.println("word5 = ( " + wordString5 + " )" + ", length of this word = ( " + wordLength5 + " )");
+
+        if (wordLength1 > wordLength2 && wordLength1 > wordLength3 && wordLength1 > wordLength4
+                && wordLength1 > wordLength5) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+        /////////////////////////
+
+        System.out.println("--------------------------");
+        String in = new String("STAR WARS: The Rise of SKywalker");
+        String[] splitSentence = in.split("[Aa]");
+        int countChar = splitSentence.length - 1;
+        System.out.println("Number of occurrences of character 'A' & 'a' : " + countChar);
+
+        // String someString = new String("STAR WARS: The Rise of SKywalker");
+        // int count = 0;
+        // for (int i = 0; i < someString.length(); i++) {
+        // if (someString.charAt(i) == 'a' || someString.charAt(i) == 'A') {
+        // count++;
+        // }
+        // }
+        // System.out.println("Number of occurrences of character 'a' & 'A' : " +
+        // count);
     }
 }
---
+++ hw-basic-java/hws/src/lesson3/hw2.md
@@ -0,0 +1,48 @@
+### Hw2 lesson 3
+
+**Working with Strings**
+
+- [Github Page](https://github.com/moahmadkamel/qa6-java/tree/master/hw-basic-java/hws/src/lesson3)
+
+---
+
+Create a variable string1 = "This line that i want to cut, cause it is too long".
+
+Create a line string2 in which the value of string1 should be placed, truncated to "This line that I want to cut, cause".
+
+Create a string string3 based on string2 that will contain the value "This line that don't want to cut, cause it is perfect".
+
+Print each message and its length to the console.
+___ 
+
+Create a string = "Testing, is my favorite job".
+
+Display each word separately and the length of this word in the following form:
+
+Word1 = (word value), Length of this word = (word length value).
+
+Word2 = (word value), Length of this word = (word length value).
+etc.
+
+Print true to the console if the first word is longer than the 
+others, in otherwise print false.
+
+---
+
+When you apply the length field to an array of strings, you can find out how many elements are in the array.
+
+That is, if you have an array of strings like: 
+
+String[] arrayOfString;
+
+then when calling the length field:
+
+int size = arrayOfString.length;
+
+you can get how many characters are in this array.
+
+Try, using the split method and information about the length field, to display how many times the character 'a' occurs in the line:
+
+"Completely random text in English. In it, we just need to determine how many times the character 'a' occurs there. And we can use the split method and the length method."
+
+Solve this problem by taking into account the upper and lower case of the character 'a'.
